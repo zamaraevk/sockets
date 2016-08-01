7b1f99a9d1442db1f4ba1b07ada686596ff2d578
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-08-01 19:11:18 -0400

    server

--- server.js
+++ server.js
@@ -15,7 +15,54 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+  socket.on('location', function(data) {
+      console.log("Incoming location:", data)
+      //sending dummy data for group update
+
+     // socket.emit('groupUpdate', {'group':[{'latitude': data.coordinates.latitude, 'longitude':  data.coordinates.longitude, 'title': 'Konst' }, {'latitude':data.coordinates.latitude + 0.0008, 'longitude': data.coordinates.longitude, 'title': 'Bo' }]});
+    });
+    socket.on('error', function(err) {
+      console.log("Error", err);
+    });
+
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+// var express = require('express')
+// app = require('./api');
+// var http = require('http')
+// var server = require('http').Server(app);
+// var port = process.env.PORT || 3001;
+// app.use(express.static('../'));
+// //var api = require("./api");
+
+// // var bodyParser = require('body-parser');
+
+// server.listen(port, ()=>
+//   console.log('server listening on port: ' + port)
+//   );
+// // use socket.io
+// var io = require('socket.io').listen(server);
+
+
+// //turn off debug
+// io.set('log level', 1);
+
+// // define interactions with client
+// io.sockets.on('connection', function(socket){
+
+//      socket.on('location', function(data) {
+//       console.log("Incoming location:", data)
+//       //sending dummy data for group update
+
+//       socket.emit('groupUpdate', {'group':[{'latitude': data.coordinates.latitude, 'longitude':  data.coordinates.longitude, 'title': 'Konst' }, {'latitude':data.coordinates.latitude + 0.0008, 'longitude': data.coordinates.longitude, 'title': 'Bo' }]});
+//     });
+//     socket.on('error', function(err) {
+//       console.log("Error", err);
+//     });
+// });
+
+// module.exports = app;
